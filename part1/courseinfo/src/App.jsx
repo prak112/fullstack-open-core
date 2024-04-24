cb409996bcd85a4f3c929556b75937732fe1124e
PrakGov <55034062+prak112@users.noreply.github.com>
2024-04-24 16:56:26 +0300

    ex 1.3-1.5 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -22,35 +22,50 @@ const Content = (props) => {
   console.log(props);
   return(
     <>
-      <Part name = { props.parts[0] } exercises = { props.exercises[0] }/>
-      <Part name = { props.parts[1] } exercises = { props.exercises[1] }/>
-      <Part name = { props.parts[2] } exercises = { props.exercises[2] }/>
+      <Part name = { props.parts[0].name } exercises = { props.parts[0].exercises }/>
+      <Part name = { props.parts[1].name } exercises = { props.parts[1].exercises }/>
+      <Part name = { props.parts[2].name } exercises = { props.parts[2].exercises }/>
     </>
   )
 }
 
 const Total = (props) => {
   console.log(props);
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
   return(
     <>
-      <p>Total number of exercises : {props.total}</p>
+      <p>Total number of exercises : { total }</p>
     </>
   )
 }
 
 
-
+// Top-level/Parent Component
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [ 'Fundamentals of React',  'Using props to pass data', 'State of a component']
-  const exercises = [ 10, 7, 14 ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <Header course = { course }/>
-      <Content parts = { parts } exercises = { exercises } />
-      <Total total = { exercises[0] + exercises[1] + exercises[2] }/>
+      <Header course = { course.name }/>
+      <Content parts = { course.parts }  />
+      <Total parts = { course.parts }/>
     </div>
   )
 }
